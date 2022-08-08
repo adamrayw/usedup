ff74fdfad6b99b714a3159013807ed6b87bb05a3
adamrayw <raywibowo68@gmail.com>
2022-08-08 20:53:51 +0700

    setup react-router-dom, add root route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.png';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Header from './components/Header';
+import Home from './pages/Home';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <FooterComponent />
     </div>
   );
 }
