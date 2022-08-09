d4a8918b8603b8b311b03ef0da333969ed7fc5e5
adamrayw <raywibowo68@gmail.com>
2022-08-09 12:20:23 +0700

    add login, register, and not found routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Home from './pages/Home';
 import FooterComponent from './components/FooterComponent';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
       <FooterComponent />
     </div>
