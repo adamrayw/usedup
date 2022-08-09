79e1689cb0bf7a4df870973b903dc33864a95498
adamrayw <raywibowo68@gmail.com>
2022-08-09 14:22:18 +0700

    Redesign profile dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
-import { Navbar, Button } from "flowbite-react"
+import { Navbar, Button, Dropdown, Avatar } from "flowbite-react"
 import logo from '../logo.png'
 import Categories from "./Categories"
 import Search from "./Search"
 import { Link } from 'react-router-dom'
+import { FaBullhorn, FaHeart, FaSignOutAlt } from 'react-icons/fa'
 
 function Header() {
     return (
@@ -11,7 +12,6 @@ function Header() {
             <Navbar
                 fluid={true}
                 rounded={true}
-                className="shadow-lg"
             >
                 <Navbar.Brand href="/">
                     <img
@@ -34,33 +34,43 @@ function Header() {
                             Jual
                         </Button>
                     </div>
-                    {/* <Dropdown
-                        arrowIcon={false}
-                        inline={true}
-                        label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
-                    >
-                        <Dropdown.Header>
-                            <span className="block text-sm">
-                                Bonnie Green
-                            </span>
-                            <span className="block truncate text-sm font-medium">
-                                name@flowbite.com
-                            </span>
-                        </Dropdown.Header>
-                        <Dropdown.Item>
-                            Dashboard
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Settings
-                        </Dropdown.Item>
-                        <Dropdown.Item>
-                            Earnings
-                        </Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item>
-                            Sign out
-                        </Dropdown.Item>
-                    </Dropdown>*/}
+                    {/* <div className="text-left">
+                        <Dropdown
+                            arrowIcon={false}
+                            inline={true}
+                            label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                        >
+                            <Dropdown.Header>
+                                <div className="flex items-center space-x-2">
+                                    <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />
+                                    <div>
+                                        <span className="block text-sm">
+                                            Hallo,
+                                        </span>
+                                        <span className="block truncate text-sm font-medium">
+                                            Adam Ray
+                                        </span>
+                                    </div>
+                                </div>
+                            </Dropdown.Header>
+                            <Dropdown.Item>
+                                <div className="flex items-center">
+                                    <FaBullhorn className="mr-2" /> Iklan Saya
+                                </div>
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                <div className="flex items-center">
+                                    <FaHeart className="mr-2" /> Favorit Saya
+                                </div>
+                            </Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item>
+                                <div className="flex items-center">
+                                    <FaSignOutAlt className="mr-2" /> Logout
+                                </div>
+                            </Dropdown.Item>
+                        </Dropdown>
+                    </div> */}
                     <Navbar.Toggle />
                 </div>
                 <Navbar.Collapse>
