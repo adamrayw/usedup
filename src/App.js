b7bb4758a9a575e8ba0ff69bac1360f07ed2b707
adamrayw <raywibowo68@gmail.com>
2022-08-09 14:26:14 +0700

    add route detail page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FooterComponent from './components/FooterComponent';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import PageNotFound from './pages/PageNotFound';
+import DetailItem from './pages/DetailItem';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/dijual-hyundai-stargazer-baru' element={<DetailItem />} />
       </Routes>
       <FooterComponent />
     </div>
