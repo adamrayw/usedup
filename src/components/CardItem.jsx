6c9abcfb3cb3da76c6c466b1b459fd337e5cce10
adamrayw <raywibowo68@gmail.com>
2022-08-09 10:36:13 +0700

    add state favorited

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -2,13 +2,32 @@ import { Card } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 import Produk from '../assets/produk.jpg'
 import { FaHeart } from 'react-icons/fa'
+import { useState } from 'react'
 
 function CardItem() {
+    const [favorite, setFavorite] = useState(false)
+
+
+    const onFavorite = () => {
+        setFavorite(!favorite)
+    }
+
     return (
         <>
             <div className="max-w-sm relative">
                 <div className="absolute top-0 right-0 pt-4 pr-4 z-50">
-                    <button className='p-2 border bg-white rounded-full'><FaHeart className='text-gray-400' /></button>
+                    {favorite ? (<>
+                        <button className='p-2 border bg-red-400 rounded-full' onClick={onFavorite}>
+
+                            <FaHeart className='text-white' />
+                        </button>
+
+                    </>) : (<>
+                        <button className='p-2 border bg-white rounded-full' onClick={onFavorite}>
+                            <FaHeart className='text-gray-400' />
+                        </button>
+                    </>)}
+
                 </div>
                 <Link to="/">
                     <Card
