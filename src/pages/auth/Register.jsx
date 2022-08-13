d6677b173e78e37a34d35f7fe0e3df9992335361
adamrayw <raywibowo68@gmail.com>
2022-08-13 14:10:35 +0700

    Integration with Register API

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,8 +1,54 @@
-import { Card, Label, TextInput, Button } from 'flowbite-react'
+import { Card, Label, TextInput, Button, Spinner } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { register, reset } from '../../features/auth/authSlice'
+import { useState, useEffect } from 'react'
 
 function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const { name, email, password } = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+
+        if (isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [user, isSuccess, isError, message, navigate, dispatch])
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const userData = {
+            name, email, password
+        }
+
+        dispatch(register(userData))
+    }
+
     return (
         <>
             <div className="login-container h-screen pt-10 px-4">
@@ -18,7 +64,7 @@ function Register() {
                             </div>
                             <p className='font-normal text-gray-400 mt-2'>Register untuk menjual, menambahkan ke favorit, dan membeli.</p>
                         </div>
-                        <form className="flex flex-col gap-4">
+                        <form className="flex flex-col gap-4" onSubmit={onSubmit}>
                             <div>
                                 <div className="mb-2 block">
                                     <Label
@@ -29,6 +75,8 @@ function Register() {
                                 <TextInput
                                     id="nama"
                                     type="text"
+                                    name='name'
+                                    onChange={onChange}
                                     required={true}
                                 />
                             </div>
@@ -42,6 +90,8 @@ function Register() {
                                 <TextInput
                                     id="email1"
                                     type="email"
+                                    name='email'
+                                    onChange={onChange}
                                     placeholder="name@usedup.com"
                                     required={true}
                                 />
@@ -56,10 +106,12 @@ function Register() {
                                 <TextInput
                                     id="password1"
                                     type="password"
+                                    name='password'
+                                    onChange={onChange}
                                     required={true}
                                 />
                             </div>
-                            <div>
+                            {/* <div>
                                 <div className="mb-2 block">
                                     <Label
                                         htmlFor="confirmPassword"
@@ -71,10 +123,22 @@ function Register() {
                                     type="password"
                                     required={true}
                                 />
-                            </div>
-                            <Button type="submit" color="dark">
-                                Register
-                            </Button>
+                            </div> */}
+                            {isLoading ? (
+                                <Button color='dark'>
+                                    <div className="mr-3">
+                                        <Spinner
+                                            size="sm"
+                                            light={true}
+                                        />
+                                    </div>
+                                    Loading ...
+                                </Button>
+                            ) : (
+                                <Button type="submit" color="dark">
+                                    Register
+                                </Button>
+                            )}
                         </form>
                     </Card>
                 </div>
