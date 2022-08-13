bc80738dcb6dfeec704ae5e69a158da1ea77a040
adamrayw <raywibowo68@gmail.com>
2022-08-13 14:09:57 +0700

    integration with auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,21 @@ import { Navbar, Button, Dropdown, Avatar } from "flowbite-react"
 import logo from '../logo.png'
 import Categories from "./Categories"
 import Search from "./Search"
-import { Link } from 'react-router-dom'
-// import { FaBullhorn, FaHeart, FaSignOutAlt } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+import { FaBullhorn, FaHeart, FaSignOutAlt } from 'react-icons/fa'
 
 function Header() {
+    const { user } = useSelector((state) => state.auth)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/login')
+    }
     return (
         <>
             <Navbar
@@ -24,55 +35,60 @@ function Header() {
                     </span>
                 </Navbar.Brand>
                 <div className="flex md:order-2">
-                    <div className="btn md:flex space-x-4 hidden">
-                        <Link to="/login">
-                            <Button color="dark">
-                                Login
-                            </Button>
-                        </Link>
-                        <Link to="/jual/pilih-kategori">
-                            <Button color="light">
-                                Jual
-                            </Button>
-                        </Link>
-                    </div>
-                    {/* <div className="text-left">
-                        <Dropdown
-                            arrowIcon={false}
-                            inline={true}
-                            label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
-                        >
-                            <Dropdown.Header>
-                                <div className="flex items-center space-x-2">
-                                    <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />
-                                    <div>
-                                        <span className="block text-sm">
-                                            Hallo,
-                                        </span>
-                                        <span className="block truncate text-sm font-medium">
-                                            Adam Ray
-                                        </span>
+                    {user ? (
+                        <div className="text-left">
+                            <Dropdown
+                                arrowIcon={false}
+                                inline={true}
+                                label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                            >
+                                <Dropdown.Header>
+                                    <div className="flex items-center space-x-2">
+                                        <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />
+                                        <div>
+                                            <span className="block text-sm">
+                                                Hallo,
+                                            </span>
+                                            <span className="block truncate text-sm font-medium">
+                                                {user.name}
+                                            </span>
+                                        </div>
                                     </div>
-                                </div>
-                            </Dropdown.Header>
-                            <Dropdown.Item>
-                                <div className="flex items-center">
-                                    <FaBullhorn className="mr-2" /> Iklan Saya
-                                </div>
-                            </Dropdown.Item>
-                            <Dropdown.Item>
-                                <div className="flex items-center">
-                                    <FaHeart className="mr-2" /> Favorit Saya
-                                </div>
-                            </Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item>
-                                <div className="flex items-center">
-                                    <FaSignOutAlt className="mr-2" /> Logout
-                                </div>
-                            </Dropdown.Item>
-                        </Dropdown>
-                    </div> */}
+                                </Dropdown.Header>
+                                <Dropdown.Item>
+                                    <div className="flex items-center">
+                                        <FaBullhorn className="mr-2" /> Iklan Saya
+                                    </div>
+                                </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <div className="flex items-center">
+                                        <FaHeart className="mr-2" /> Favorit Saya
+                                    </div>
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>
+                                    <div className="flex items-center" onClick={onLogout}>
+                                        <FaSignOutAlt className="mr-2" /> Logout
+                                    </div>
+                                </Dropdown.Item>
+                            </Dropdown>
+                        </div>
+                    ) : (
+                        <div className="btn md:flex space-x-4 hidden">
+                            <Link to="/login">
+                                <Button color="dark">
+                                    Login
+                                </Button>
+                            </Link>
+                            <Link to="/jual/pilih-kategori">
+                                <Button color="light">
+                                    Jual
+                                </Button>
+                            </Link>
+                        </div>
+
+                    )}
+
                     <Navbar.Toggle />
                 </div>
                 <Navbar.Collapse>
