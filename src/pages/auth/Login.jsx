b720a3f0ebfdba8a59f18df263a5e1419e46ae1f
adamrayw <raywibowo68@gmail.com>
2022-08-13 14:10:25 +0700

    Integration with Login API

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,15 +1,68 @@
-import { Card, Label, TextInput, Button } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Card, Label, TextInput, Button, Spinner } from 'flowbite-react'
+import { Link, useNavigate } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
+import { useSelector, useDispatch } from 'react-redux'
+import { login, reset } from '../../features/auth/authSlice'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { user, isError, isLoading, isSuccess, message } = useSelector((state) => state.auth)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+
+        if (isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isSuccess, message, message, navigate, dispatch])
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const { email, password } = formData
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const userData = {
+            email, password
+        }
+
+        dispatch(login(userData))
+    }
+
+
+
     return (
         <>
             <div className="login-container h-screen pt-10 px-4">
                 <div className="max-w-sm mx-auto text-left">
                     <Card>
                         <div>
-
                             <div className='flex justify-between items-center'>
                                 <h2 className='font-bold text-3xl'>LOGIN</h2>
                                 <Link to="/register" className='flex items-center font-medium text-blue-800'>
@@ -18,7 +71,7 @@ function Login() {
                             </div>
                             <p className='font-normal text-gray-400 mt-2'>Ingin mencari sesuatu? banyak barang bagus loh!</p>
                         </div>
-                        <form className="flex flex-col gap-4">
+                        <form className="flex flex-col gap-4" onSubmit={onSubmit}>
                             <div>
                                 <div className="mb-2 block">
                                     <Label
@@ -29,6 +82,8 @@ function Login() {
                                 <TextInput
                                     id="email1"
                                     type="email"
+                                    name='email'
+                                    onChange={onChange}
                                     placeholder="name@usedup.com"
                                     required={true}
                                 />
@@ -43,12 +98,26 @@ function Login() {
                                 <TextInput
                                     id="password1"
                                     type="password"
+                                    name='password'
+                                    onChange={onChange}
                                     required={true}
                                 />
                             </div>
-                            <Button type="submit" color="dark">
-                                Login
-                            </Button>
+                            {isLoading ? (
+                                <Button color='dark'>
+                                    <div className="mr-3">
+                                        <Spinner
+                                            size="sm"
+                                            light={true}
+                                        />
+                                    </div>
+                                    Loading ...
+                                </Button>
+                            ) : (
+                                <Button type="submit" color="dark">
+                                    Login
+                                </Button>
+                            )}
                         </form>
                     </Card>
                 </div>
