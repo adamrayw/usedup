6d2e9930d9b802e0b74fc5b78fd267804607ca63
adamrayw <raywibowo68@gmail.com>
2022-08-13 14:10:09 +0700

    auth service and slice

---
+++ src/features/auth/authService.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const API_URL = '/api/'
+
+
+// Register User
+const register = async (userData) => {
+    const response = await axios.post(API_URL + 'register', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+// Login User
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+// logout
+const logout = async () => {
+    localStorage.removeItem('user')
+}
+
+const authService = {
+    register,
+    login,
+    logout
+}
+
+export default authService
