2f32f3a25fa830fe6ae18d1ecf546af9e5a6b9b7
adamrayw <raywibowo68@gmail.com>
2022-08-15 22:48:11 +0700

    add form reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
+import formReducer from '../features/form/formSlice'
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        form: formReducer
     },
 })
 
