98553f1e90258c656302099c86569cf3e08bbcd3
adamrayw <raywibowo68@gmail.com>
2022-08-16 13:02:19 +0700

    SuccessPages

---
+++ src/pages/form-jual/SuccessPages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SuccessPages() {
+    return (
+        <div>Terima Kasih sudah menjual barang anda, saat ini kami sedang me-review barang kamu, demi kenyamanan bersama</div>
+    )
+}
+
+export default SuccessPages
