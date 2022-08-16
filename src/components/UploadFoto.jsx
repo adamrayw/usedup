fd44e871cbade6e4324256a1e5e06e2a6713d1fd
adamrayw <raywibowo68@gmail.com>
2022-08-16 12:59:39 +0700

    UploadFoto Component

---
+++ src/components/UploadFoto.jsx
@@ -0,0 +1,35 @@
+import { FaPlus } from 'react-icons/fa'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { uploadFile } from '../features/form/formSlice'
+
+function UploadFoto() {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+
+  }, [dispatch])
+
+
+  function selectImage(e) {
+    if (e.target.files && e.target.files.length > 0) {
+      dispatch(uploadFile(e.target.files[0]))
+    }
+
+  }
+
+  return (
+    <div id="fileUpload" className='flex flex-wrap justify-between'>
+      <div>
+        <label htmlFor="image">
+          <div className='border border-black p-4 inline-block'>
+            <FaPlus />
+          </div>
+        </label>
+        <input className='hidden' type="file" name="image" id="image" onChange={selectImage} />
+      </div>
+    </div>
+  )
+}
+
+export default UploadFoto
