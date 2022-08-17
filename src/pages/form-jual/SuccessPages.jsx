b615b051c93700033e3547ce729ede1857b37626
adamrayw <raywibowo68@gmail.com>
2022-08-17 23:26:28 +0700

    redesign success page

--- src/pages/form-jual/SuccessPages.jsx
+++ src/pages/form-jual/SuccessPages.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import Success from '../../assets/success.svg'
 function SuccessPages() {
     return (
-        <div>Terima Kasih sudah menjual barang anda, saat ini kami sedang me-review barang kamu, demi kenyamanan bersama</div>
+        <div className='max-w-xl mx-auto flex flex-col justify-center items-center my-16 space-y-6 px-4'>
+            <img src={Success} alt="success" className='w-2/5' />
+            <div className='space-y-2'>
+                <h1 className='font-bold text-xl'>FORM BERHASIL TERKIRIM</h1>
+                <p className='font-medium'>Terima Kasih sudah menjual, saat ini kami sedang me-review barang kamu.</p>
+            </div>
+        </div>
     )
 }
 
