bd54db13d990d851a34fa0bd9b00b05aecab87ab
adamrayw <raywibowo68@gmail.com>
2022-08-18 21:38:40 +0700

    add header Authorization

--- src/features/form/formService.js
+++ src/features/form/formService.js
@@ -2,8 +2,17 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:8080/api/'
 
-const formMobilBekas = async (data) => {
-    const response = await axios.post(API_URL + 'mobil-bekas', data)
+const user = localStorage.getItem('user')
+
+const formMobilBekas = async (data, token) => {
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.post(API_URL + 'mobil-bekas', data, config)
 
     return response.data
 }
