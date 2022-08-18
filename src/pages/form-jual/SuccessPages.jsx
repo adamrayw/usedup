378fffa793763d00ae067b4d204de2c3151eb8d3
adamrayw <raywibowo68@gmail.com>
2022-08-18 21:39:27 +0700

    add link

--- src/pages/form-jual/SuccessPages.jsx
+++ src/pages/form-jual/SuccessPages.jsx
@@ -5,7 +5,8 @@ function SuccessPages() {
             <img src={Success} alt="success" className='w-2/5' />
             <div className='space-y-2'>
                 <h1 className='font-bold text-xl'>FORM BERHASIL TERKIRIM</h1>
-                <p className='font-medium'>Terima Kasih sudah menjual, saat ini kami sedang me-review barang kamu.</p>
+                <p className='font-medium text-gray-400'>Terima Kasih sudah menjual, saat ini kami sedang me-review barang kamu.</p>
+                <a href="/" className='inline-block underline pt-6'>Kembali ke Home</a>
             </div>
         </div>
     )
