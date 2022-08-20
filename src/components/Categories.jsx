94be3143cd30ee9b6b32f41ece7fd3a64d139f9a
adamrayw <raywibowo68@gmail.com>
2022-08-20 19:37:26 +0700

    integrate categories API

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,36 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from "axios"
+import { toast } from "react-toastify"
 
 function Categories() {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+
+    }, [])
+
+    const getCategories = async () => {
+        try {
+            // const response = await axios.get('http://localhost:8080/api/kategori')
+            const response = await axios.get('https://usedup.herokuapp.com/api/kategori')
+
+            setCategories(response.data)
+        } catch (error) {
+            toast.error(error)
+        }
+    }
     return (
         <>
             <div className='bg-white pt-4 pb-3 mb-1 shadow-sm w-full text-center'>
                 <div className='flex md:overflow-x-hidden md:px-0 px-2 overflow-x-scroll space-x-6 md:justify-center '>
-                    <Link to='/' className='whitespace-nowrap'>Mobil Bekas</Link>
-                    <Link to='/' className='whitespace-nowrap'>Motor Bekas</Link>
-                    <Link to='/' className='whitespace-nowrap'>Property</Link>
-                    <Link to='/' className='whitespace-nowrap'>Elektronik & Gadget</Link>
-                    <Link to='/' className='whitespace-nowrap'>TV & Audio, Video</Link>
+                    {categories.map((e) => {
+                        return (
+                            <Link key={e.id} to={e.slug} className='whitespace-nowrap hover:underline'>{e.name}</Link>
+                        )
+                    })}
                 </div>
             </div>
         </>
