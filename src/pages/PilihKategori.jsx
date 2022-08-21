d3c447c386aac4c327c8ff6486ef943475e5146f
adamrayw <raywibowo68@gmail.com>
2022-08-21 21:06:21 +0700

    now Pilih kategori integration kategori API

--- src/pages/PilihKategori.jsx
+++ src/pages/PilihKategori.jsx
@@ -1,7 +1,26 @@
 import { Card } from 'flowbite-react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function PilihKategori() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+
+    }, [])
+
+    const getCategories = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/kategori')
+
+            setCategories(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="max-w-lg mx-auto container py-10 md:px-0 px-4">
@@ -10,26 +29,28 @@ function PilihKategori() {
                         <h1 className='text-4xl font-bold'>Pilih Kategori</h1>
                         <small className='text-gray-400'>Pilih kategori sesuai barang yang ingin kamu jual</small>
                     </div>
-                    <Link to="/jual/mobil-bekas">
-                        <div className='w-full p-4 border border-black hover:bg-gray-50 transition'>
-                            Mobil Bekas
-                        </div>
-                    </Link>
-                    <Link to="/jual/motor-bekas">
-                        <div className='w-full p-4 border border-black hover:bg-gray-50 transition'>
-                            Motor Bekas
-                        </div>
-                    </Link>
-                    <Link to="/jual/property">
-                        <div className='w-full p-4 border border-black hover:bg-gray-50 transition'>
-                            Property
-                        </div>
-                    </Link>
-                    <Link to="/jual/elektronik-gadget">
-                        <div className='w-full p-4 border border-black hover:bg-gray-50 transition'>
-                            Elektronik & Gadget
-                        </div>
-                    </Link>
+                    {categories.length === 0 ? (
+                        <>
+                            <div role="status" className="animate-pulse">
+                                <div className="h-10 bg-gray-300 dark:bg-gray-700 max-w-[640px] mb-2.5 mx-auto"></div>
+                                <div className="h-10 bg-gray-300 dark:bg-gray-700 max-w-[640px] mb-2.5 mx-auto"></div>
+                                <div className="h-10 bg-gray-300 dark:bg-gray-700 max-w-[640px] mb-2.5 mx-auto"></div>
+                                <div className="h-10 bg-gray-300 dark:bg-gray-700 max-w-[640px] mb-2.5 mx-auto"></div>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            {categories.map((e) => {
+                                return (
+                                    <Link key={e.id} to={'/jual/' + e.slug}>
+                                        <div className='w-full p-4 border border-black hover:bg-gray-50 transition'>
+                                            {e.name}
+                                        </div>
+                                    </Link>
+                                )
+                            })}
+                        </>
+                    )}
                 </Card>
             </div>
         </>
