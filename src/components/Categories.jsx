958f517ed19c76f5b97ed90e7d651098b713a2f7
adamrayw <raywibowo68@gmail.com>
2022-08-23 12:28:01 +0700

    add skeleton loading and fix class attribute

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -27,11 +27,22 @@ function Categories() {
         <>
             <div className='bg-white pt-4 pb-3 mb-1 shadow-sm w-full text-center'>
                 <div className='flex md:overflow-x-hidden md:px-0 px-2 overflow-x-scroll space-x-6 md:justify-center '>
-                    {categories.map((e) => {
-                        return (
-                            <Link key={e.id} to={e.slug} className='whitespace-nowrap hover:underline'>{e.name}</Link>
-                        )
-                    })}
+                    {categories.length === 0 ? (
+                        <>
+                            <div className="h-2 bg-gray-200 rounded-full animate-pulse w-10 dark:bg-gray-700"></div>
+                            <div className="h-2 bg-gray-200 rounded-full animate-pulse w-10 dark:bg-gray-700"></div>
+                            <div className="h-2 bg-gray-200 rounded-full animate-pulse w-10 dark:bg-gray-700"></div>
+                        </>
+                    ) : (
+                        <>
+                            {categories.map((e) => {
+                                return (
+                                    <Link key={e.id} to={e.slug} className='whitespace-nowrap hover:underline'>{e.name}</Link>
+                                )
+                            })}
+                        </>
+                    )
+                    }
                 </div>
             </div>
         </>
