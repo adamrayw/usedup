fb69ae96d05aa3a5b64beda7ad14bf94e5bf1469
adamrayw <raywibowo68@gmail.com>
2022-08-24 12:19:27 +0700

    fix issue on mobile screen

--- src/components/ImageFull.jsx
+++ src/components/ImageFull.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 function ImageFull(props) {
     console.log(props.image)
     return (
-        <div id="defaultModal" tabindex="-1" aria-hidden="true" class=" overflow-y-auto overflow-x-hidden fixed bg-black bg-opacity-80 z-10 w-full flex justify-center items-center md:inset-0 h-modal md:h-full">
-            <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <img src={URL.createObjectURL(props.image)} alt="preview" />
-                </div>
+        <div id="defaultModal" tabindex="-1" aria-hidden="true" class=" overflow-y-auto overflow-x-hidden fixed bg-black bg-opacity-80 z-10 w-full flex justify-center items-center inset-0 h-full">
+            <div class="relative p-4 w-full max-w-2xl h-auto">
+                <img src={URL.createObjectURL(props.image)} className="w-auto" alt="preview" />
             </div>
         </div>
     )
