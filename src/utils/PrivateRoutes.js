9d7fd7b7f2e92501d85a58eae98da07b9146974d
adamrayw <raywibowo68@gmail.com>
2022-08-24 10:33:28 +0700

    dispatch resetUpload when page change

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,8 +1,13 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { resetUpload } from '../features/form/formSlice'
+import { useDispatch } from 'react-redux'
 
 const PrivateRoutes = () => {
     const { user } = useSelector((state) => state.auth)
+    const dispatch = useDispatch()
+
+    dispatch(resetUpload())
 
     return (
         user ? <Outlet /> : <Navigate to='/login' />
