c9279151c0d9c8acadb3e620f9740fd40549e5fd
adamrayw <raywibowo68@gmail.com>
2022-08-30 22:07:43 +0700

    add indicator if profile not complete

--- src/components/AvatarProfile.jsx
+++ src/components/AvatarProfile.jsx
@@ -2,17 +2,36 @@ import { Avatar } from 'flowbite-react'
 import { useSelector } from 'react-redux'
 function AvatarProfile() {
     const { user } = useSelector((state) => state.auth)
+    console.log(user)
     return (
         <>
-            {user.no_telp === null ? (
-                <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}
-                    status='busy'
-                    statusPosition='top-right'
-                />
-            ) : (
-                <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}
+            {user.foto_profile ? (
+                <>
+                    {user.no_telp === null ? (
+                        <Avatar alt="User settings" img={user.foto_profile.secure_url} rounded={true}
+                            status='busy'
+                            statusPosition='top-right'
+                        />
+                    ) : (
+                        <Avatar alt="User settings" img={user.foto_profile.secure_url} rounded={true}
 
-                />
+                        />
+                    )}
+                </>
+            ) : (
+                <>
+                    {user.no_telp === null ? (
+                        <Avatar alt="User settings" rounded={true}
+                            status='busy'
+                            statusPosition='top-right'
+                        />
+                    ) : (
+                        <Avatar alt="User settings" rounded={true}
+
+                        />
+                    )
+                    }
+                </>
             )}
         </>
     )
