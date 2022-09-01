f8cd3931b7a9ee92e38b278529e728a7d4774420
adamrayw <raywibowo68@gmail.com>
2022-09-01 22:01:31 +0700

    add new condition

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -49,7 +49,7 @@ export default function Dropdown() {
                                 </div>
                             )}
                             <div className="flex items-center space-x-3 my-2">
-                                {user.foto_profile !== null ? (
+                                {user.foto_profile !== null && user.foto_profile !== '' ? (
                                     <img alt="User settings" src={user.foto_profile.secure_url} className='w-10 h-10 rounded-full bg-cover' rounded={true} />
                                 ) : (
                                     <svg class="h-10 bg-gray-100 rounded-full w-auto text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
