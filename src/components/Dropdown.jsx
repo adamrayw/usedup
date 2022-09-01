35d2f352f21ada234369fcaf58bf64f4dabeb609
adamrayw <raywibowo68@gmail.com>
2022-09-02 00:12:11 +0700

    centering profile dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -18,9 +18,9 @@ export default function Dropdown() {
         navigate('/login')
     }
     return (
-        <div className="">
+        <div className="flex items-center">
             <Menu as="div" className="relative inline-block text-left">
-                <div>
+                <div className="flex items-center">
                     <Menu.Button className="inline-flex w-full justify-center rounded-md text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
                         <AvatarProfile />
                     </Menu.Button>
