3b2728b0a2535c19dab2219a6c8e40bdc1503541
adamrayw <raywibowo68@gmail.com>
2022-09-05 12:33:13 +0700

    SuccessVerified Page

---
+++ src/pages/SuccessVerif.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import api from '../utils/api'
+import { useParams } from 'react-router-dom'
+import { MdVerified } from 'react-icons/md'
+import { Spinner } from 'flowbite-react'
+import { Link } from 'react-router-dom'
+
+function SuccessVerif() {
+    const [status, setStatus] = useState(false)
+
+    useEffect(() => {
+        updateIsVerified()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const { id } = useParams()
+
+    const updateIsVerified = async () => {
+        try {
+            const response = await axios.post(api + '/verification?id=' + id)
+            setStatus(response.data.status)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <div className='h-96 flex flex-col items-center justify-center space-y-4'>
+            {status ? (
+                <>
+                    <MdVerified size='8em' className='text-green-400' />
+                    <h1 className='px-2 text-gray-800 md:text-2xl text-xl font-bold'>Akun kamu telah diverifikasi!</h1>
+                    <Link to='/' className='text-gray-400 underline'>
+                        Kembali ke Beranda
+                    </Link>
+                </>
+            ) : (
+                <Spinner color='info' size='lg' />
+            )}
+        </div>
+    )
+}
+
+export default SuccessVerif
