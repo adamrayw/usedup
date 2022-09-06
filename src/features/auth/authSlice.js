a0005700343a92718e08bf164c493b2b134ce360
adamrayw <raywibowo68@gmail.com>
2022-09-06 21:08:27 +0700

    check if user available or not

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -6,7 +6,7 @@ const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
     user: user ? user : null,
-    isVerified: user.isVerified,
+    isVerified: user ? user.isVerified : false,
     isError: false,
     isSuccess: false,
     isLoading: false,
