91fa069231d54e6362cfaa7fbd1e579bedb597bc
adamrayw <raywibowo68@gmail.com>
2022-09-15 20:16:17 +0700

    change height and width of AvatarProfile

--- src/components/AvatarProfile.jsx
+++ src/components/AvatarProfile.jsx
@@ -23,11 +23,11 @@ function AvatarProfile() {
                     {user.no_telp === null ? (
                         <div className='relative'>
                             <div className='absolute right-0 w-2.5 h-2.5 rounded-full bg-red-500'></div>
-                            <FaUserCircle className=' text-gray-200 rounded-full w-10 h-10' />
+                            <FaUserCircle className=' text-gray-200 rounded-full md:w-10 w-8 md:h-10 h-8' />
                         </div>
 
                     ) : (
-                        <FaUserCircle className=' text-gray-200 rounded-full w-10 h-10' />
+                        <FaUserCircle className=' text-gray-200 rounded-full md:w-10 w-8 md:h-10 h-8' />
                     )
                     }
                 </>
