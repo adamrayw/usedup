d41da687bfd31b3db09cf258077ba41f1f50067f
adamrayw <raywibowo68@gmail.com>
2022-09-19 19:59:27 +0700

    redesign iklan saya

--- src/pages/IklanSaya.jsx
+++ src/pages/IklanSaya.jsx
@@ -2,7 +2,7 @@ import { Tabs } from 'flowbite-react'
 import axios from 'axios'
 import api from '../utils/api'
 import { useEffect, useState } from 'react'
-import { FaHeart, FaEye } from 'react-icons/fa'
+import { FaHeart, FaEye, FaRegEdit, FaTrash, FaRegEyeSlash } from 'react-icons/fa'
 
 function IklanSaya() {
     const [data, setData] = useState([])
@@ -63,27 +63,49 @@ function IklanSaya() {
                                         {data.map((e, index) => {
                                             return (
                                                 <div key={index} className='container border border-gray-200 px-5 py-4'>
-                                                    <div className='flex space-x-4'>
-                                                        <img src={e.foto[0].url} className='w-18 h-10' alt="iklanimage" />
-                                                        <div className='text-left'>
-                                                            <h2 className='md:text-lg text-sm font-bold tracking-tight text-gray-900 text-left dark:text-white line-clamp-2'>{e.judul_iklan}</h2>
-                                                            <div className='flex items-center space-x-4 mt-2'>
-                                                                <p className='md:text-sm text-xs mt-1 text-left font-bold text-blue-600 dark:text-white'>
-                                                                    Rp {Intl.NumberFormat('id-ID').format(e.harga)}
-                                                                </p>
-                                                                <div className='flex items-center space-x-4'>
-                                                                    <div className='flex items-center space-x-1.5'>
-                                                                        <FaHeart className='text-sm text-red-400' />
-                                                                        <p className='text-xs text-gray-300 font-medium'>{e.Favorit.length} difavorit</p>
-                                                                    </div>
-                                                                    <div className='flex items-center space-x-1.5'>
-                                                                        <FaEye className='text-sm text-gray-400' />
-                                                                        <p className='text-xs text-gray-300 font-medium'>{e.dilihat}x dilihat</p>
+                                                    <div className='flex justify-between items-center md:flex-row flex-col'>
+                                                        <div className='flex space-x-4 '>
+                                                            <img src={e.foto[0].url} className='w-24 h-auto' alt="iklanimage" />
+                                                            <div className='text-left flex items-center justify-between'>
+                                                                <div>
+                                                                    <h2 className='md:text-lg text-sm font-bold tracking-tight text-gray-900 text-left dark:text-white line-clamp-2'>{e.judul_iklan}</h2>
+                                                                    <div className='flex md:items-center items-start md:flex-row flex-col md:space-x-4 space-y-2 md:mt-2'>
+                                                                        <p className='md:text-sm text-xs mt-1 md:w-32 text-left font-bold text-blue-600 dark:text-white'>
+                                                                            Rp {Intl.NumberFormat('id-ID').format(e.harga)}
+                                                                        </p>
+                                                                        <div className='flex items-center space-x-4'>
+                                                                            <div className='flex items-center space-x-1.5'>
+                                                                                <FaHeart className='text-sm text-red-400' />
+                                                                                <p className='text-xs text-gray-300 font-medium'>{e.Favorit.length}</p>
+                                                                            </div>
+                                                                            <div className='flex items-center space-x-1.5'>
+                                                                                <FaEye className='text-sm text-gray-400' />
+                                                                                <p className='text-xs text-gray-300 font-medium'>{e.dilihat}x dilihat</p>
+                                                                            </div>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
+
+                                                            </div>
+                                                        </div>
+                                                        <div>
+                                                            <div className='flex md:flex-row items-center space-x-2 flex-wrap md:mt-0 mt-3'>
+                                                                <button className='flex items-center text-xs text-white px-2 py-2 bg-yellow-300'>
+                                                                    <FaRegEdit className='md:block hidden mr-2' />
+                                                                    Edit Iklan
+                                                                </button>
+                                                                <button className='flex items-center text-xs bg-red-400 text-white  px-2 py-2 '>
+                                                                    <FaTrash className='md:block hidden mr-2' />
+                                                                    Hapus Iklan
+                                                                </button>
+                                                                <button className='flex items-center text-xs bg-gray-400 text-white px-2 py-2 '>
+                                                                    <FaRegEyeSlash className='md:block hidden mr-2' />
+                                                                    Nonaktifkan sementara
+                                                                </button>
                                                             </div>
                                                         </div>
                                                     </div>
+
                                                 </div>
                                             )
                                         })}
