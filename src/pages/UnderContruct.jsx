c03aa790ff3e6612fb60878e63e8f10bca74ef7d
adamrayw <raywibowo68@gmail.com>
2022-10-03 16:12:29 +0700

    update

--- src/pages/UnderContruct.jsx
+++ src/pages/UnderContruct.jsx
@@ -8,10 +8,10 @@ function UnderContruct() {
                 <div className="featured my-20">
                     <div className='mb-4 space-y-8 text-center'>
                         <img src={UnderConstruct} className="md:w-80 w-56 mx-auto" alt="logo" />
-                        <h2 className='font-bold md:text-2xl text-xl text-gray-800'>Maaf, website dapat diakses kembali pada tanggal 1 Oktober 2022</h2>
-                        {/* <Link to='/' className='text-sm underline block text-gray-400 hover:text-gray-600 transition'>
+                        <h2 className='font-bold md:text-2xl text-xl text-gray-800'>Maaf, halaman ini sedang dalam pengembangan</h2>
+                        <Link to='/' className='text-sm underline block text-gray-400 hover:text-gray-600 transition'>
                             Kembali ke home
-                        </Link> */}
+                        </Link>
                     </div>
                 </div>
             </div>
