6111fb3fc561278560f7dc9820a8ed3e4f92144c
adamrayw <raywibowo68@gmail.com>
2022-11-10 08:22:39 +0700

    Add onSearch method

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Search() {
     const [keyword, setKeyword] = useState()
+
+    const navigate = useNavigate()
 
     function setKeywords(e) {
         e.preventDefault()
@@ -10,16 +12,24 @@ function Search() {
         console.log(keyword);
     }
 
+    function onSearch() {
+        if (keyword.length === 0) {
+            return
+        } else {
+            navigate(`/search/${keyword}`)
+        }
+    }
+
     return (
         <>
             <form>
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300" required>Search</label>
                 <div className="relative md:ml-16">
                     <input type="text" id="default-search" className="block p-3 pl-4 pr-8 md:w-96 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 placeholder-gray-300 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari mobil, motor, handphone, dan lainnya..." required="" onChange={setKeywords} value={keyword} onKeyDown={(e) => { e.key === 'Enter' && e.preventDefault() }} />
-                    <div className="flex absolute inset-y-0 right-0 items-center px-3">
-                        <Link to={`/search/${keyword}`} className='w-5 h-5'>
-                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        </Link>
+                    <div className="flex absolute inset-y-0 right-0 items-center px-3 bg-gray-200 hover:cursor-pointer rounded-r-lg transition-all active:bg-slate-300" onClick={onSearch}>
+                        {/* <Link to={`/search/${keyword}`} className='w-5 h-5'> */}
+                        <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        {/* </Link> */}
                     </div>
                 </div>
             </form>
