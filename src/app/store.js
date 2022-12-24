700ab3f8727bca602cc8fdc10f1c7851149f6d63
adamrayw <raywibowo68@gmail.com>
2022-12-24 22:19:06 +0700

    add chat reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
 import formReducer from '../features/form/formSlice'
+import chatReducer from '../features/chat/chatSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        form: formReducer
+        form: formReducer,
+        chat: chatReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
