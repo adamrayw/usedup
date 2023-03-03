658daecb2fabe744fc1bba15f67767dca62cc7ee
adamrayw <raywibowo68@gmail.com>
2023-03-04 00:01:11 +0700

    change socket endpoint

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-// const socket = io("https://usedup-backend.up.railway.app")
-const socket = io("http://localhost:3001")
+const socket = io("https://usedup-backend.up.railway.app")
+// const socket = io("http://localhost:3001")
 
 export default socket
